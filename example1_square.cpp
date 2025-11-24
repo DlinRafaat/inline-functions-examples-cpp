3ef08a6e2619b7f2bcff0fecb6d9e2c00a4361d1
DlinRafaat <dlin.rafaat@hotmail.com>
2025-11-23 22:44:16 -0600

    Add comments for inline function usage in square.cpp
    Added comments explaining the use of inline function for squaring a number.

--- example1_square.cpp
+++ example1_square.cpp
@@ -10,3 +10,8 @@ int main() {
     cout << "Square of 10 is: " << square(10) << endl;
     return 0;
 }
+
+
+// This program uses an inline function called square() that returns the value of a number multiplied by itself. 
+// Because it is marked as inline, the compiler can insert the expression x * x directly into the code whenever square() is called. In the main function, the program calls square(5) and square(10) and prints the results. 
+// This example shows how inline functions are useful for small, repeated calculations that benefit from faster execution.
